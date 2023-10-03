398b738368376715abad6633e0e18cb64680fb31
Muccachino <lucas_staszewski@yahoo.de>
2023-10-03 11:10:13 +0200

    first commit, API test

---
+++ src/index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+import "./styles.scss";
+import {
+  createTags,
+  createMultipleTags,
+  expandHtml,
+} from "./modules/tag_functions";
+
+const content = document.getElementById("app");
+
+/* const getRecipe = async () => {
+  const url =
+    "https://gustar-io-deutsche-rezepte.p.rapidapi.com/search_api?text=K%C3%A4se";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "1f98616057msh05389b767bd090fp133c7bjsn34cb1de9bfa7",
+      "X-RapidAPI-Host": "gustar-io-deutsche-rezepte.p.rapidapi.com",
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+    createMultipleTags("div", result.length - 1, content, result, "recipe");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const recipeList = getRecipe(); */
---
+++ src/modules/tag_functions.js
@@ -0,0 +1,40 @@
+"use strict";
+
+function createTags(htmlTags, className, idName, innerHtml) {
+  let element = document.createElement(htmlTags);
+  if (className != null) {
+    element.classList.add(className);
+  }
+  if (idName != null) {
+    element.id = idName;
+  }
+  if (innerHtml != null) {
+    element.innerHTML = innerHtml;
+  }
+  return element;
+}
+
+function expandHtml(expand, tags) {
+  expand.appendChild(tags);
+}
+
+function addPicture(picImportName, parent) {
+  let myPic = new Image();
+  myPic.src = picImportName;
+  parent.appendChild(myPic);
+}
+
+function createMultipleTags(node, amount, parent, innerHtml, className) {
+  for (let i = 0; i <= amount; i++) {
+    let tag = document.createElement(node);
+    if (innerHtml != null) {
+      tag.innerHTML = innerHtml[i].title;
+    }
+    if (className != null) {
+      tag.classList.add(`${className}${i}`);
+    }
+    parent.appendChild(tag);
+  }
+}
+
+export { createTags, expandHtml, addPicture, createMultipleTags };
